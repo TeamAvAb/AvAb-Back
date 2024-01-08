0249ea19c527e8f2f7b9f2ac31f45f3904f900b2
eomgerm <fishbread00@gmail.com>
2024-01-08 21:15:32 +0900

    :rocket: Init Project

---
+++ src/main/java/com/avab/avab/AvAbApplication.java
@@ -0,0 +1,13 @@
+package com.avab.avab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvAbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AvAbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/avab/avab/AvAbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avab.avab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvAbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
