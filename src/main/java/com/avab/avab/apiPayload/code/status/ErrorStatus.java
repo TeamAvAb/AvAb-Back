fb58562b37fe038f0d691730e0411029784931b3
88dldl <chltnrgml0999@gmail.com>
2024-01-10 14:31:47 +0900

    :sparkles: API 응답통일/ 에러 핸들링

--- src/main/java/com/avab/avab/apiPayload/code/status/ErrorStatus.java
+++ src/main/java/com/avab/avab/apiPayload/code/status/ErrorStatus.java
@@ -15,10 +15,7 @@ public enum ErrorStatus implements BaseErrorCode {
     _BAD_REQUEST(HttpStatus.BAD_REQUEST, "COMMON400", "잘못된 요청입니다."),
     _UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "COMMON401", "인증이 필요합니다."),
     _FORBIDDEN(HttpStatus.FORBIDDEN, "COMMON403", "금지된 요청입니다."),
-
-    // test용
-    TEMP_EXCEPTION(HttpStatus.BAD_REQUEST, "TEMP4001", "avab 테스트");
-
+    ;
     private final HttpStatus httpStatus;
     private final String code;
     private final String message;
