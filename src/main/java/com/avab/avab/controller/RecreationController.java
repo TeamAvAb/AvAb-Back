f0bdaaf712a5d84ad135edcf3141ddff5202d685
88dldl <chltnrgml0999@gmail.com>
2024-01-18 05:10:18 +0900

    :sparkles: 레크레이션 상세설명 API 구현

--- src/main/java/com/avab/avab/controller/RecreationController.java
+++ src/main/java/com/avab/avab/controller/RecreationController.java
@@ -2,17 +2,16 @@ package com.avab.avab.controller;
 
 import java.util.List;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.avab.avab.apiPayload.BaseResponse;
+import com.avab.avab.dto.recreation.RecreationResponseDTO.DescriptionDTO;
 import com.avab.avab.dto.recreation.RecreationResponseDTO.PopularRecreationListDTO;
 import com.avab.avab.service.RecreationService;
 
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -24,12 +23,20 @@ public class RecreationController {
 
     @Operation(summary = "인기 레크레이션 목록 조회 API", description = "조회수를 기준으로 인기 레크레이션 목록을 조회합니다.")
     @ApiResponses({
-        @ApiResponse(
-                responseCode = "COMMON200",
-                description = "OK, 성공"),
+        @ApiResponse(responseCode = "COMMON200", description = "OK, 성공"),
     })
     @GetMapping("/popular")
     public BaseResponse<List<PopularRecreationListDTO>> getTop3RecreationsByViewCount() {
         return BaseResponse.onSuccess(recreationService.getTop3RecreationsByViewCount());
+    }
+
+    @Operation(summary = "레크레이션 상세설명 조회 API", description = "레크레이션 상세설명을 조회합니다.")
+    @ApiResponses({
+        @ApiResponse(responseCode = "COMMON200", description = "OK, 성공"),
+    })
+    @GetMapping("/description/{recreationId}")
+    public BaseResponse<DescriptionDTO> getRecreationDescription(
+            @PathVariable(name = "recreationId") Long recreationId) {
+        return BaseResponse.onSuccess(recreationService.getRecreationDescription(recreationId));
     }
 }
