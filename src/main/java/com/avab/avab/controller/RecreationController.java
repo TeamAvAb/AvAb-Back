8b501737c6c409b99a7f98e9ad623cd7e68984fe
88dldl <chltnrgml0999@gmail.com>
2024-01-18 05:45:59 +0900

    :sparkles: 레크레이션 상세설명 API 구현

--- src/main/java/com/avab/avab/controller/RecreationController.java
+++ src/main/java/com/avab/avab/controller/RecreationController.java
@@ -10,20 +10,21 @@ import com.avab.avab.dto.recreation.RecreationResponseDTO.PopularRecreationListD
 import com.avab.avab.service.RecreationService;
 
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/api/recreations")
 @RequiredArgsConstructor
 public class RecreationController {
-
     private final RecreationService recreationService;
 
     @Operation(summary = "인기 레크레이션 목록 조회 API", description = "조회수를 기준으로 인기 레크레이션 목록을 조회합니다.")
     @ApiResponses({
-        @ApiResponse(responseCode = "COMMON200", description = "OK, 성공"),
+            @ApiResponse(
+                    responseCode = "COMMON200",
+                    description = "OK, 성공"),
     })
     @GetMapping("/popular")
     public BaseResponse<List<PopularRecreationListDTO>> getTop3RecreationsByViewCount() {
