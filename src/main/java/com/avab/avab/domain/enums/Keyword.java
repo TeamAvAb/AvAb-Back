9b45db120868880820278dcc404e8893b97ff664
eomgerm <fishbread00@gmail.com>
2024-01-26 19:02:34 +0900

    :sparkles: [Feat]: 키워드 추가

--- src/main/java/com/avab/avab/domain/enums/Keyword.java
+++ src/main/java/com/avab/avab/domain/enums/Keyword.java
@@ -2,8 +2,13 @@ package com.avab.avab.domain.enums;
 
 public enum Keyword {
     QUICKNESS,
+    SENSIBLE,
+    COOPERATIVE,
+    ACTIVE,
     BRAIN,
-    COOPERATION,
-// TODO: 요구사항에 따라 더 추가할 것
-;
+    PSYCHOLOGICAL,
+    LUCK,
+    COMMON_SENSE,
+    PREPARATION,
+    ;
 }
