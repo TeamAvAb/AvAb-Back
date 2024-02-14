05fe6ca3fc76d44dcc5ce7d8dbf682c9d5d6f9e0
88dldl <chltnrgml0999@gmail.com>
2024-02-14 16:03:47 +0900

    :sparkles: [Feat] 레크레이션 대표 이미지 추가

--- src/main/java/com/avab/avab/converter/RecreationConverter.java
+++ src/main/java/com/avab/avab/converter/RecreationConverter.java
@@ -159,6 +159,7 @@ public class RecreationConverter {
                                                 (recreationFavorite ->
                                                         recreationFavorite.getUser().equals(user)))
                                 : null)
+                .ImageUrl(recreation.getImageUrl())
                 .build();
     }
 
