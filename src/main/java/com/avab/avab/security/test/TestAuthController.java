8767db03cce0c66aaf30b35228b8003849b4a66a
eomgerm <fb_26.55@gmail.com>
2024-08-27 09:48:39 +0900

    :recycle: [Refactor]: 테스트 로그인 롤백

--- src/main/java/com/avab/avab/security/test/TestAuthController.java
+++ src/main/java/com/avab/avab/security/test/TestAuthController.java
@@ -1,10 +1,12 @@
 package com.avab.avab.security.test;
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.avab.avab.apiPayload.BaseResponse;
+import com.avab.avab.security.test.dto.LoginRequest;
 import com.avab.avab.security.test.dto.LoginResponse;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -19,7 +21,7 @@ public class TestAuthController {
     private final TestAuthService authService;
 
     @PostMapping("/login")
-    public BaseResponse<LoginResponse> login() {
-        return BaseResponse.onSuccess(authService.login("tester1@test.com"));
+    public BaseResponse<LoginResponse> login(@RequestBody LoginRequest request) {
+        return BaseResponse.onSuccess(authService.login(request.getEmail()));
     }
 }
