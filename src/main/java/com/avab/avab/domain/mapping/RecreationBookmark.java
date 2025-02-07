a86b8971265667ace7cfcc2de88a526c35ab74da
88dldl <chltnrgml0999@gmail.com>
2025-02-07 14:38:37 +0900

    :sparkles: [Feat] 즐겨찾기 엔티티 생성 및 연관관계 추가

---
+++ src/main/java/com/avab/avab/domain/mapping/RecreationBookmark.java
@@ -0,0 +1,33 @@
+package com.avab.avab.domain.mapping;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+import com.avab.avab.domain.Recreation;
+import com.avab.avab.domain.User;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class RecreationBookmark {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "recreation_id")
+    private Recreation recreation;
+}
