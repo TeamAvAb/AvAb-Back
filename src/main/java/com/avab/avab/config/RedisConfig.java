46f223910a81dc009b5dd25262f17a82caf52624
eomgerm <fishbread00@gmail.com>
2025-03-19 16:45:17 +0900

    :ambulance: [HOTFIX]: Redis ssl 연결 추가

--- src/main/java/com/avab/avab/config/RedisConfig.java
+++ src/main/java/com/avab/avab/config/RedisConfig.java
@@ -3,6 +3,8 @@ package com.avab.avab.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
@@ -17,7 +19,12 @@ public class RedisConfig {
 
     @Bean
     public LettuceConnectionFactory connectionFactory() {
-        return new LettuceConnectionFactory(host, port);
+        RedisStandaloneConfiguration redisConfig = new RedisStandaloneConfiguration(host, port);
+
+        LettuceClientConfiguration clientConfig =
+                LettuceClientConfiguration.builder().useSsl().build();
+
+        return new LettuceConnectionFactory(redisConfig, clientConfig);
     }
 
     @Bean
